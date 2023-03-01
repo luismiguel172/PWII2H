4c3303e303f379ee747ec8b5d166d312b3bcab06
luismiguel172 <luis.goncalves68@etec.sp.gov.br>
2023-03-01 19:49:56 -0300

    Adicionando novos arquivos

---
+++ aulaphp.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Aula de PHP 01/03</title>
+</head>
+<body>
+
+	<<?php 
+
+	$data = "01 de março de 2023";
+	$salario = 1250;
+	$cargo = "Estagiário";
+	$idade = 20;
+	$resultado = true;
+
+	if ($resultado){
+		echo "Verdadeiro";
+	}
+
+	 ?>
+
+</body>
+</html>
