e99e0d23ed10de25100c50511df8a05064041f02
luismiguel172 <luis.goncalves68@etec.sp.gov.br>
2023-04-05 20:19:49 -0300

    atualização abril

--- atividade abril.php
+++ atividade abril.php
@@ -1,14 +1,6 @@
 <?php
 
-    $num1 = 15;
-    $num2 = 10;
-    $op = 1;
-    $res = 0;
-
-    echo "Escolha dois números <br>";
-    echo "Escolha a operação: <br> soma <br> subtracao <br> multiplicacao <br> divisao <br> <br>";
-
-    function soma($num1, $num2, $res){
+     function soma($num1, $num2, $res){
 
         $res = $num1 + $num2;
         return $res;
@@ -27,37 +19,52 @@
 
         $res = $num1/$num2;
         return $res;
-    }function imprimir(){
-        echo "Operacao selecionada: soma <br>";
+    
+    }function imprimir($num1, $num2, $op, $res){
+        
+        echo "Operacao selecionada: $op <br>";
         echo "Numeros selecionados: $num1 e $num2 <br>";
+        soma($res);
+
     }
+
+    //while($op != 5){
+
+    echo "Bem vindo ao sistema <br> <br>";
+    echo "Escolha dois números <br> <br>";
+    echo "Escolha a operação: <br> 1 - soma <br> 2 - subtracao <br> 3 - multiplicacao <br> 4 - divisao <br> <br>";
+    echo "Pressione 5 para sair do sistema <br> <br>";
+
+    $num1 = 15;
+    $num2 = 10;
+    $op = 1;
+    $res = 0;
 
     switch($op){
         case 1:
-         echo "Operacao selecionada: soma <br>";
-         echo "Numeros selecionados: $num1 e $num2 <br>";
-
          soma($num1, $num2, $res);
+         imprimir($num1, $num2, $op, $res); 
          break;
          
-        case 2: 
-         echo "Operacao selecionada: subtracao <br>";
-         echo "Numeros selecionados: $num1 e $num2 <br>";
+        case 2:
+         imprimir($num1, $num2, $op);
          subtracao($num1, $num2, $res);
          break;   
 
         case 3:
-         echo "Operacao selecionada: multiplicacao <br>";
-         echo "Numeros selecionados: $num1 e $num2 <br>";
+         imprimir($num1, $num2, $op);   
          multiplicacao($num1, $num2, $res);
 
-        case 4: 
-         echo "Operacao selecionada: divisao <br>";
-         echo "Numeros selecionados: $num1 e $num2 <br>";
+        case 4:
+         imprimir($num1, $num2, $op);   
          divisao($num1, $num2, $res);
          break;   
+
+        case 5:
+            echo "Saindo do sistema"; 
+            break;
 
         default: 
          echo "Escolha uma operação";
         }    
-
+    
